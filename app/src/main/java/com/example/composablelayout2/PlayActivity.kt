8194d47df31e49220bec56a1f2ca69a9a608a327
elwinspranata <elwinpranata02@gmail.com>
2024-10-25 01:44:08 +0700

    selesai

--- app/src/main/java/com/example/composablelayout2/PlayActivity.kt
+++ app/src/main/java/com/example/composablelayout2/PlayActivity.kt
@@ -3,6 +3,7 @@ package com.example.composablelayout2
 import android.preference.PreferenceActivity.Header
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,9 +17,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,64 +32,106 @@ import org.w3c.dom.Text
 
 @Preview(showBackground = true)
 @Composable
-fun PlayActivity() {
-    Column{modifier = Modifier.fillMaxSize()){
-         HeaderSection(){
-             Text(text = "Kepada YTH,", modifier = Modifier.padding(16.dp))
-             Text(text = "Elwins Pranata", modifier = Modifier.padding(start = 16.dp))
-             Spacer(modifier = Modifier.size(40.dp))
-             DetailSurat(
-                 judul = "Nama",
-                 isinya = "Elwins Pranata"
-             )
-             DetailSurat(
-                 judul = "Alamat",
-                 isinya = "Kota Bandung,"
-             )
-             DetailSurat(
-                 judul = "No Telp",
-                 isinya = "0987654"
-             )
-             DetailSurat(
-                 judul = "Kepentingan",
-                 isinya = "Detail"
-         }
+fun PlayActivity(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        HeaderSection()
+        Text(text = "Kepada YTH,", modifier = Modifier.padding(16.dp))
+        Text(text = "Elwins Pranata", modifier = Modifier.padding(start = 16.dp))
+        Spacer(modifier = Modifier.size(40.dp))
+        DetailSurat(
+            judul = "Nama",
+            isinya = "Elwins Pranata"
+        )
+        DetailSurat(
+            judul = "Alamat",
+            isinya = "Kota Bandung,"
+        )
+        DetailSurat(
+            judul = "No Telp",
+            isinya = "0987654"
+        )
+        DetailSurat(
+            judul = "Kepentingan",
+            isinya = "DetailSurat(\n" +
+                    "            judul = \"Nama\",\n" +
+                    "            isinya = \"Ihsan Putra Riadi\"\n" +
+                    "        )\n" +
+                    "        DetailSurat(\n" +
+                    "            judul = \"Alamat\",\n" +
+                    "            isinya = \"Kota Banten, \"\n" +
+                    "        )\n" +
+                    "        DetailSurat(\n" +
+                    "            judul = \"No Telp\",\n" +
+                    "            isinya = \"081311641569 \"\n" +
+                    "        )"
+        )
+    }
+}
+
+@Composable
+fun HeaderSection() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.Gray)
+            .padding(15.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Text(
+                    text = "Daerah Istimewa Yogyakarta",
+                    color = Color.White
+                )
+                Text(
+                    text = "FAX : 021-121212, TLP : 08745678",
+                    color = Color.White
+                )
+            }
             Box(
-                modifier = Modifier.fillMaxWidth()
-                    .background(color = Color.Gray)
-                    .padding(15.dp)
-
-
+                contentAlignment = Alignment.BottomStart
             ) {
-                Row {
-                    Column {
-                        Text(
-                            text = "DIY",
-                            color = Color.Blue
-                        )
-
-                        Text(
-                            text = "Fax : 087732690",
-                            color = Color.White
-                        )
-
-                    }
-                    Box(
-                        Image(
-                            painter = painterResource(id = R.drawable.ucl),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(100.dp)
-                                .clip(CircleShape)
-                        )
-
-
-                                      contentDescription = null
-                                      )
-                }
+                Image(
+                    painter = painterResource(id = R.drawable.elwin),
+                    contentDescription = "",
+                    Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                )
+                Image(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = null
+                )
             }
         }
+    }
+}
 
-
-
-
+@Composable
+fun DetailSurat(
+    judul: String,
+    isinya: String
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.size(50.dp))
+        Row(modifier = Modifier.fillMaxWidth().padding(top = 2.dp)) {
+            Text(
+                text = judul,
+                modifier = Modifier.weight(0.8f)
+            )
+            Text(
+                text = ":",
+                modifier = Modifier.weight(0.8f)
+            )
+            Text(
+                text = isinya,
+                modifier = Modifier.weight(2f)
+            )
+        }
+    }
+}
